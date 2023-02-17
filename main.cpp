8b19d92bcf16b56090bb2c49591348984cc1fbb5
Konishi, Hiroto <39662862+HiroKoni@users.noreply.github.com>
2023-02-17 14:36:09 +0900

    Simplify main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+/* This is a sample program of SCPP. */
+
 #include "scpp/scpp_ast.hpp"
 #include "scpp/scpp_evaluater.hpp"
 #include <iostream>
@@ -7,71 +9,27 @@ int main(int argc, char const *argv[])
     using std::cin;
     using std::cout;
     using namespace SCPP;
-    Expr i = tInt(3);
-    Expr j = tInt(4);
-    Expr k = tAdd(i, j);
-    Expr l = tSub(k, j);
-    Expr m = tAdd(
-        tMul(
-            i,
-            tInt(4)),
-        tDiv(
-            tInt(5),
-            tInt(6)));
-    Expr n = tLt(
-        tInt(3),
-        tAdd(
-            tInt(4),
-            tInt(5)));
-    // Expr o = tAssign(
-    //     "a",
-    //     tInt(3));
-    // Expr p = tSeq(tAssign("a", tInt(3)), tAssign("b", tInt(4)), tAssign("a", tAdd(tIdent("a"), tIdent("b"))), tIdent("a"));
-    Expr q = tIf(
-        tLt(
-            tInt(1650),
-            tAdd(
-                tInt(4),
-                tInt(5))),
-        tAssign(
-            "a",
-            tInt(3)));
-    Expr r = tWhile(
-        tLt(
-            tIdent("a"),
-            tInt(3)),
-        tSeq(
-            tAssign(
-                "a",
-                tAdd(
-                    tIdent("a"),
-                    tInt(1))),
-            tAssign(
-                "b",
-                tAdd(
-                    tIdent("b"),
-                    tInt(15)))));
-    auto Program = tProgram(
+
+    auto expression = tAdd(tInt(1),tInt(5)); /* 1+5 */
+    cout << evaluate(expression) << std::endl; /* evaluate the expression */
+
+    auto program = tProgram(
         FunctionList(
             tFunction(
-                "add",
-                ParamList("a", "b"),
-                tAdd(
-                    tIdent("a"),
-                    tIdent("b"))) /* add(a,b){return a+b} */
-            ),
-        tAssign("a", tInt(3)),                                          /* a=3 */
-        tAssign("b", tInt(4)),                                          /* b=4 */
-        tAssign("a", tCall("add", tIdent("a"), tIdent("b"))), /* a=add(a,b) */
-        tAssign("c", tInt(10)),                                         /* c=10 */
-        tCall("add", tIdent("a"), tIdent("c"))/* add(a,c) */);
+                "isOdd",
+                ParamList("a"),
+                tIf(tEq(tMod(tIdent("a"), tInt(2)), tInt(0)), tInt(0), tInt(1))),
+            tFunction(
+                "isEven",
+                ParamList("a"),
+                tIf(tEq(tMod(tIdent("a"), tInt(2)), tInt(0)), tInt(1), tInt(0)))),
+        tAssign("a", tInt(231)),
+        tAssign("b", tInt(150)),
+        tAssign("c", tCall("isOdd", tIdent("a"))),
+        tAssign("d", tCall("isEven", tIdent("b"))),
+        tAnd(tIdent("c"), tIdent("d"))); /* function isOdd(a){if(a%2==0) return 0; else return 1;} function isEven(a){if(a%2==0) return 1; else return 0;} a = 231; b = 150; c = isOdd(a); d = isEven(b); c && d; */
 
-    cout << evaluate(i) << evaluate(j) << std::endl;
-    // cout << evaluate(k) << evaluate(l) << evaluate(m) << evaluate(n) << evaluate(o) << std::endl;
-    // cout << evaluate(p) << std::endl;
-    cout << evaluate(q) << std::endl;
-    cout << evaluate(r) << std::endl;
-    cout << evaluateProgram(Program) << std::endl;
+    cout << evaluateProgram(program) << std::endl;/* evaluate the program */
 
     return 0;
 }
